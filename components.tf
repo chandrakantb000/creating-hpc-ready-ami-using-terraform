60d0b71ad36056dc27f189b65222c50988581f67
Chandrakant <83269908+nightfury000@users.noreply.github.com>
2025-12-19 17:28:36 +0530

    added terraform variables.tf and providers.tf

--- components.tf
+++ components.tf
@@ -4,4 +4,9 @@ resource "aws_imagebuilder_component" "update_os_component" {
   version               = "1.0.0"
   supported_os_versions = ["Amazon Linux 2023", "Ubuntu 22.04", "Ubuntu 20.04", "RHEL 8", "RHEL 9"]
   data                  = file("./components/update-os.yaml")
+    
+
+
+
+
 }
