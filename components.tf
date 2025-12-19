c1344d6f06ac3564734f00fd86f4c4169961959b
Chandrakant <83269908+nightfury000@users.noreply.github.com>
2025-12-19 17:18:02 +0530

    added update_os_component in components.tf file

---
+++ components.tf
@@ -0,0 +1,7 @@
+resource "aws_imagebuilder_component" "update_os_component" {
+  name                  = "update-os-component"
+  platform              = "Linux"
+  version               = "1.0.0"
+  supported_os_versions = ["Amazon Linux 2023", "Ubuntu 22.04", "Ubuntu 20.04", "RHEL 8", "RHEL 9"]
+  data                  = file("./components/update-os.yaml")
+}
