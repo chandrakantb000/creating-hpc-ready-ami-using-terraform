cf5c2fe3fbc8f5f67166a89f19f39877948f75ff
Chandrakant <83269908+nightfury000@users.noreply.github.com>
2025-12-23 23:44:49 +0530

    started with optimize-performance component

--- components.tf
+++ components.tf
@@ -7,12 +7,12 @@ resource "aws_imagebuilder_component" "update_os_component" {
     
 }
 
-# resource "aws_imagebuilder_component" "optimize_image_component" {
-#   name                  = "optimize-image-component"
+# resource "aws_imagebuilder_component" "optimize_performance_component" {
+#   name                  = "optimize-performance-component"
 #   platform              = "Linux"
 #   version               = "1.0.0"
 #   supported_os_versions = ["Amazon Linux 2023", "Ubuntu 22.04", "Ubuntu 20.04", "RHEL 8", "RHEL 9"]
-#   data                  = file("./components/optimize-image.yaml")
+#   data                  = file("./components/optimize-performance.yaml")
   
 # }
 
