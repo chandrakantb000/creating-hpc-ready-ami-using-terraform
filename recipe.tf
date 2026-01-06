b0fbbe9a78b3a87b3723c0fd522551200c2b7dc6
Chandrakant <83269908+nightfury000@users.noreply.github.com>
2026-01-06 16:07:43 +0530

    added block_device_mapping block that increases the volume size, as spack gave error due to no space on the device

--- recipe.tf
+++ recipe.tf
@@ -10,6 +10,21 @@ resource "aws_imagebuilder_image_recipe" "test_all_components_recipe" {
   description  = "Test ImageBuilder recipe for all components"
   parent_image = "ami-0c5ddb3560e768732" # Ubuntu 22.04 ami-id (us-east-2)
 
+  # While installing spack component image build fails with "No space left on device" error on default ubuntu ami root volume size of 8GB.
+  # So increasing the root volume size to 50GB.
+  block_device_mapping {
+    device_name = "/dev/xvdb"
+
+    ebs {
+      delete_on_termination = true
+      volume_size = 40
+      volume_type = "gp3"
+    }
+  }
+
+
+  #
+
 # Below we defined the sequence of components to be applied to the base image
 
   component {
@@ -58,7 +73,9 @@ resource "aws_imagebuilder_image_recipe" "test_all_components_recipe" {
     component_arn = aws_imagebuilder_component.spack_install_component.arn
   }
 
-
+# added lifecycle policy to reverses the default terraform behavior of destroying and recreating the resource on changes to certain attributes.
+# Now when changes are made to the recipe, terraform will create the new version before destroying the old one. And this version will be referenced by the pipeline.
+# Suppose if we want to keep the previous recipe version  then we can add "retain_previous_version = true" inside lifecycle block.
     lifecycle {
     create_before_destroy = true
   }
